61c00f1cfd76a3400d950c51353c1a511ec081dd
keishiF <kf.syukatsu@gmail.com>
2026-09-07 12:25:30 +0900

    Dxライブラリの追加

---
+++ FIC_GameJam_Summer/FIC_GameJam_Summer/game.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// ‰æ–Êî•ñ‚ğ’è”’è‹`
+constexpr int kScreenWidth = 1280;
+constexpr int kScreenHeight = 720;
+constexpr int kColorBItNum = 32;
+
+constexpr bool kDefaultWindowMode = true;
